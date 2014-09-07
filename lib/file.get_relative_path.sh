4b302d2a02fd4beb981bfb9821f59cebce0bfa71
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-09-07 22:41:57 +0200

    Added function file.get_relative_path to lib.

---
+++ lib/file.get_relative_path.sh
@@ -0,0 +1,30 @@
+# Function for computing the relative path of two given paths.
+#
+# @author    See http://stackoverflow.com/a/14914070
+# @copyright See http://stackoverflow.com/a/14914070
+# @license   See http://stackoverflow.com/a/14914070
+
+function file.get_relative_path () {
+    [ $# -ge 1 ] && [ $# -le 2 ] || return 1
+    current="${2:+"$1"}"
+    target="${2:-"$1"}"
+    [ "$target" != . ] || target=/
+    target="/${target##/}"
+    [ "$current" != . ] || current=/
+    current="${current:="/"}"
+    current="/${current##/}"
+    appendix="${target##/}"
+    relative=''
+    while appendix="${target#"$current"/}"
+        [ "$current" != '/' ] && [ "$appendix" = "$target" ]; do
+        if [ "$current" = "$appendix" ]; then
+            relative="${relative:-.}"
+            echo "${relative#/}"
+            return 0
+        fi
+        current="${current%/*}"
+        relative="$relative${relative:+/}.."
+    done
+    relative="$relative${relative:+${appendix:+/}}${appendix#/}"
+    echo "$relative"
+}
