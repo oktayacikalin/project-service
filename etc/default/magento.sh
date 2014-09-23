ac01690f7b9adf8d0c4529ae5c758379ab432a4a
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-09-23 05:33:43 +0200

    Changed magento defaults to search its scripts in the service root dir so that they won't have to be symlinked explicitely.

--- etc/default/magento.sh
+++ etc/default/magento.sh
@@ -1,7 +1,7 @@
 var.default MAGENTO_HOME "${BASE_DIR}/shop"
 var.default MAGENTO_SHELL_INDEXER "shell/indexer.php"
-var.default MAGENTO_SHELL_MIGRATE "${BASE_DIR}/bin/magento-migrate.php"
-var.default MAGENTO_SHELL_EXPORT_TRANSLATIONS "${BASE_DIR}/bin/magento-export-inline-translations.php"
+var.default MAGENTO_SHELL_MIGRATE "${SERVICE_ROOT_DIR}/scripts/magento-migrate.php"
+var.default MAGENTO_SHELL_EXPORT_TRANSLATIONS "${SERVICE_ROOT_DIR}/scripts/magento-export-inline-translations.php"
 var.default MAGE_FILE "${MAGENTO_HOME}/app/code/core/Mage/Core/Model/App.php"
 var.default MAGE_COMMANDS "
     Mage_Core_Model_Resource_Setup::applyAllUpdates
