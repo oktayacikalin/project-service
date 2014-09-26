f36749f71da1dc71fa022440580f2ff3480f4ccf
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-09-26 22:57:17 +0200

    Implemented mysql.get_database_size.

---
+++ lib/mysql.get_database_size.sh
@@ -0,0 +1,17 @@
+# Function for estimating the size of a database.
+#
+# @author    Oktay Acikalin <oktay.acikalin@gmail.com>
+# @copyright Oktay Acikalin
+# @license   MIT (LICENSE.txt)
+
+function mysql.get_database_size () {
+    local connection_params=$1
+    local name=$2
+    # FIXME: Estimated size is slightly too small (about 10%).
+    sql="
+    SELECT SUM(data_length + index_length)
+    FROM information_schema.TABLES
+    WHERE table_schema='${name}' GROUP BY table_schema;
+    "
+    mysql.shell_cmd ${connection_params} -AN <<< "${sql}"
+}
