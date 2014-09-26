32c2748a18cb496aa78ce8ed3aba76dd085ccfed
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-09-26 22:17:11 +0200

    Improved service.get_commands to also support | in case statements and also support the SERVICE_ROOT_DIR var when searching for possible commands.

--- lib/service.get_commands.sh
+++ lib/service.get_commands.sh
@@ -6,8 +6,8 @@
 
 function service.get_commands () {
     local service=$1
-    local pattern_a="'[^']+'"
-    local pattern_b='"[^"]+"'
+    local pattern_a="'['A-Za-z0-9 |-]+'"
+    local pattern_b='"["A-Za-z0-9 |-]+"'
     local squote="'"
     local dquote='"'
     local commands=()
@@ -18,13 +18,14 @@ function service.get_commands () {
             base=($base)
             local base=$(tr -d '"' <<< ${base[1]} | tr -d "'")
             local source=${base//\$\{SERVICE_DIR\}/${SERVICE_DIR}}
+            source=${source//\$\{SERVICE_ROOT_DIR\}/${SERVICE_ROOT_DIR}}
             local base_commands=($(grep -oE "^\s+($pattern_a|$pattern_b)\)" $source |
                                  sed "s![$squote$dquote]!!g" | tr -d ' ' | cut -d')' -f1))
             commands+=(${base_commands[@]})
         done
     fi
     local local_commands=($(grep -oE "^\s+($pattern_a|$pattern_b)\)" $service |
-                          sed "s![$squote$dquote]!!g" | tr -d ' ' | cut -d')' -f1))
+                          sed "s![$squote$dquote]!!g" | tr -d ' ' | tr '|' ' ' | cut -d')' -f1))
     commands+=(${local_commands[@]})
     array.unique "${commands[@]}"
 }
