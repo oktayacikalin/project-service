1651a4ec99805b5c6829f49b32213cd00f3b537f
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-09-29 08:19:54 +0200

    Added file header to lib/var.default.sh.

--- lib/var.default.sh
+++ lib/var.default.sh
@@ -1,3 +1,9 @@
+# Function for setting a variable to a default value if not already set.
+#
+# @author    Oktay Acikalin <oktay.acikalin@gmail.com>
+# @copyright Oktay Acikalin
+# @license   MIT (LICENSE.txt)
+
 function var.default () {
     [ -z "${!1}" ] && declare -rg "${1}=$2"
 }
