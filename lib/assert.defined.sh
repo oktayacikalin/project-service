efd7af6848f77d482cd851328f3c1ae294ee3ee0
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-09-29 09:06:15 +0200

    Added file header to lib/assert.defined.sh.

--- lib/assert.defined.sh
+++ lib/assert.defined.sh
@@ -1,3 +1,10 @@
+# Function which checks a variable by given name and exits with code 1 and given
+# message if it's empty or does not exist.
+#
+# @author    Oktay Acikalin <oktay.acikalin@gmail.com>
+# @copyright Oktay Acikalin
+# @license   MIT (LICENSE.txt)
+
 function assert.defined () {
     [ -z "${!1}" ] && {
         echo "$2"
