347168c196f0d4b3f2aceb8314420739c653e726
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-10-02 01:35:18 +0200

    Ensured that var.init_env will only set its values once.

--- lib/var.init_env.sh
+++ lib/var.init_env.sh
@@ -4,14 +4,16 @@
 # @copyright Oktay Acikalin
 # @license   MIT (LICENSE.txt)
 
-BASE_DIR="$(readlink -f ${SERVICE_DIR}/..)"
-BIN_DIR="${BASE_DIR}/bin"
-ETC_DIR="${BASE_DIR}/etc"
-VAR_DIR="${BASE_DIR}/var"
-LIB_DIR="${BASE_DIR}/var/lib"
-LOG_DIR="${BASE_DIR}/var/log"
-PID_DIR="${BASE_DIR}/var/run"
-CACHE_DIR="${BASE_DIR}/var/cache"
-BACKUP_DIR="${BASE_DIR}/var/backups"
+if [ -z "${IMPORT_PATH}" ]; then
+    BASE_DIR="$(readlink -f ${SERVICE_DIR}/..)"
+    BIN_DIR="${BASE_DIR}/bin"
+    ETC_DIR="${BASE_DIR}/etc"
+    VAR_DIR="${BASE_DIR}/var"
+    LIB_DIR="${BASE_DIR}/var/lib"
+    LOG_DIR="${BASE_DIR}/var/log"
+    PID_DIR="${BASE_DIR}/var/run"
+    CACHE_DIR="${BASE_DIR}/var/cache"
+    BACKUP_DIR="${BASE_DIR}/var/backups"
 
-COMMAND="$1"
+    COMMAND="$1"
+fi
