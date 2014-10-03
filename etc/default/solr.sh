a0bcdc9dcf92e233cc7dfc80aa0140937d1276d3
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-10-04 01:40:53 +0200

    Code cleanup in solr.sh.

--- etc/default/solr.sh
+++ etc/default/solr.sh
@@ -1,7 +1,7 @@
 var.default SOLR_INIT_SCRIPT "${SERVICE_ROOT_DIR}/scripts/jetty-no-daemon"
 var.default SOLR_HOME "${BASE_DIR}/solr"
 var.default SOLR_HOST "${BASE_DOMAIN}"
-var.default SOLR_PORT "9002"
+var.default SOLR_PORT 9002
 var.default SOLR_PID "${PID_DIR}/solr"
 var.default SOLR_LOGS "${LOG_DIR}/solr"
 var.default SOLR_DOWNLOAD_URL "http://archive.apache.org/dist/lucene/solr/3.6.2/apache-solr-3.6.2.tgz"
