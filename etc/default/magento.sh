fc7dbaf1f5eb339162729aaa439deb1bc6cd51ff
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-10-04 13:47:56 +0200

    Cleaned up code for enabling and disabling auto migrations in the magento.service.

--- etc/default/magento.sh
+++ etc/default/magento.sh
@@ -3,8 +3,10 @@ var.default MAGENTO_CRON_PHP "cron.php"
 var.default MAGENTO_SHELL_INDEXER "shell/indexer.php"
 var.default MAGENTO_SHELL_MIGRATE "${SERVICE_ROOT_DIR}/scripts/magento-migrate.php"
 var.default MAGENTO_SHELL_EXPORT_TRANSLATIONS "${SERVICE_ROOT_DIR}/scripts/magento-export-inline-translations.php"
-var.default MAGE_FILE "${MAGENTO_HOME}/app/code/core/Mage/Core/Model/App.php"
-var.default MAGE_COMMANDS "
+var.default MAGENTO_MIGRATION_FILES "
+	app/code/core/Mage/Core/Model/App.php
+"
+var.default MAGENTO_MIGRATION_COMMANDS "
     Mage_Core_Model_Resource_Setup::applyAllUpdates
     Mage_Core_Model_Resource_Setup::applyAllDataUpdates
 "
