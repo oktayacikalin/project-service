e30830b8939c59f1fb3c1f392187a93beba0ba5f
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-10-17 22:56:14 +0200

    Added increased bulk insert buffer size to the import optimizations file.

--- scripts/mysql-import-optimizations.sql
+++ scripts/mysql-import-optimizations.sql
@@ -2,3 +2,4 @@ SET @@session.unique_checks = 0;
 SET @@session.foreign_key_checks = 0;
 SET @@session.tx_isolation = 'READ-UNCOMMITTED';
 SET @@session.sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET @@session.bulk_insert_buffer_size = 268435456;
