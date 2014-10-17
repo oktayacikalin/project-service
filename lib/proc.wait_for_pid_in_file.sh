9ee95bbe8773c89a13691ce0daa6ad74b0ada02a
Oktay Acikalin <oktay.acikalin@gmail.com>
2014-10-17 23:05:22 +0200

    Added linefeed after output of proc.wait_for_pid_in_file.

--- lib/proc.wait_for_pid_in_file.sh
+++ lib/proc.wait_for_pid_in_file.sh
@@ -7,11 +7,13 @@
 function proc.wait_for_pid_in_file () {
     local pid_file="$1"
     local timeout="${2:-30}"
+    local got_dots=0
     local i
     for (( i=0; i < ${timeout}; i++ )); do
         if [ ! -f "${pid_file}" ]; then
             sleep 1
             echo -n "."
+            got_dots=1
         fi
     done
 
@@ -29,5 +31,9 @@ function proc.wait_for_pid_in_file () {
         fi
         sleep 1
         echo -n "."
+        got_dots=1
     done
+    if [ "${got_dots}" = 1 ]; then
+        echo -n " "
+    fi
 }
